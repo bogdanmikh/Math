53a233a4ecf861da4bb20e85de07bff0388a9342
Bogdan <bogdanmikh2007@gmail.com>
2023-03-11 17:52:02 +0300

    Fixed

--- math.cpp
+++ math.cpp
@@ -3,7 +3,8 @@
 #include <cassert>
 
 #define assertm(exp, msg) assert((msg, exp))
-#define ASSERT assertm(a.size() > 0 && b.size() > 0 && correct_data(a) && correct_data(b), "INCORRECT DATA");
+#define ASSERT assertm(correct_data(a) && correct_data(b), "INCORRECT DATA");
+
 std::string Math::sum(std::string &a, std::string &b) {
     ASSERT;
     if (b.size() > a.size()) {
@@ -16,6 +17,7 @@ std::string Math::sum(std::string &a, std::string &b) {
     int index_a = a.size() - 1;
     int index_b = b.size() - 1;
     for (; index_a >= 0 || remainder != 0; index_a--, index_b--) {
+        if (a[index_a] == '-' || b[index_b] == '-') continue;
         if (index_a < 0 && remainder != 0) {
             ans.push_back(trans_to_char(remainder % 10));
             remainder /= 10;
@@ -43,24 +45,35 @@ std::string Math::sum(std::string &a, std::string &b) {
             }
        } 
     }
+    if (ans[0] == '0') {
+        for (int i = 0; ans[i] == '0' && i < ans.size(); i++) {
+            ans[i] = '#';
+        }
+    }
     std::string result;
-    for (int i = ans.size() - 1; i >= 0; i--) result.push_back(ans[i]);
+    for (int i = ans.size() - 1; i >= 0; i--) if (ans[i] != '#') result.push_back(ans[i]); 
     return result;
 }
-// std::string Math::difference(const std::string &a, const std::string &b) {
-//     if (b.size() > a.size()) {
-//         std::swap(a, b);
-//     } else if (a.size() == )
-// }
+std::string Math::difference(std::string &a, std::string &b) {
+    ASSERT;
+    if (a[0] == '-' && b[0] == '-') {
+        std::string result = Math::sum(a, b);
+        return "-" + result;  
+    }
+    return "Ilov";
+}
+
 // std::string Math::multiplication(const std::string &a, const std::string &b);
 
 // проверяет на коректность данных
-bool Math::correct_data (const std::string &a) {
-    std::string x = "-1234567890";
-    for (int i = 0; i < a.size(); i++) {
+bool Math::correct_data(const std::string &a) {
+    if (a.size() <= 0) return false;
+    bool minus = (a[0] == '-') ? true : false;
+    std::string x = "1234567890";
+    for (int i = (minus) ? 1 : 0; i < a.size(); i++) {
         bool is_finded = false;
         for (int j = 0; j < x.size(); j++) {
-            if (a[i] == x[j]) {
+            if (a[i] == x[j]) { 
                 is_finded = true;
                 break;
             }
